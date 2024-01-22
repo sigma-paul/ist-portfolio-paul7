053916040c2b961dc3f9c2fffe7e253a5ba912e2
Paul <2027brown.paul@fcboe.org>
2024-01-22 15:41:55 -0500

    code

---
+++ c/basics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This is my c basics file\n");
+
+	int num;
+
+	printf("Enter a number: ");
+	scanf("%i", &num);
+	printf("You entered %i\n", num);
+	return 0;
+}	
